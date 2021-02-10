ff99a9a469476d4558fb2b72183ea509bfbc66cb
todo-actions <todo-actions[bot]@users.noreply.github.com>
2021-02-10 06:57:36 +0000

    Collect TODO comments

--- src/datasources/venus.ts
+++ src/datasources/venus.ts
@@ -7,32 +7,32 @@ class VenusAPI<TContext = any> extends DataSource {
     }
 
     async getTotalSupplyBalance(address: string): Promise<string> {
-        // TODO: return supply balance from venus by account
+        // TODO [$602383e0d6b71309a5721587]: return supply balance from venus by account
         return '';
     }
 
     async getTotalBorrowBalance(address: string): Promise<string> {
-        // TODO: return borrow balance from venus by account
+        // TODO [$602383e0d6b71309a5721588]: return borrow balance from venus by account
         return '';
     }
 
     async getSuppliedTokens(address: string): Promise<VenusToken[]> {
-        // TODO: return all of supplied tokens by address
+        // TODO [$602383e0d6b71309a5721589]: return all of supplied tokens by address
         return [];
     }
 
     async getBorrowedTokens(address: string): Promise<VenusToken[]> {
-        // TODO: return all of borrowed tokens by address
+        // TODO [$602383e0d6b71309a572158a]: return all of borrowed tokens by address
         return [];
     }
 
     async getSupportSupplyTokens(): Promise<VenusToken[]> {
-        // TODO: return all of supported supply tokens
+        // TODO [$602383e0d6b71309a572158b]: return all of supported supply tokens
         return [];
     }
 
     async getSupportBorrowTokens(): Promise<VenusToken[]> {
-        // TODO: return all of supported borrow tokens
+        // TODO [$602383e0d6b71309a572158c]: return all of supported borrow tokens
         return [];
     }
 }
