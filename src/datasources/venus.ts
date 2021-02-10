3f8edd5527b5c46d6dcb7f59b076ed31bef921e3
todo-actions <todo-actions[bot]@users.noreply.github.com>
2021-02-10 08:01:47 +0000

    Collect TODO comments

--- src/datasources/venus.ts
+++ src/datasources/venus.ts
@@ -27,7 +27,7 @@ class VenusAPI<TContext = any> extends DataSource {
     }
 
     async getVAIMintedAmount(address: string): Promise<string> {
-        // TODO: return vai minted amount
+        // TODO [$602392ebac80a20d57b7e23d]: return vai minted amount
         return '0'
     }
 
